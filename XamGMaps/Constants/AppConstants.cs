777097d5f7d047cd2457c6a13ee7014a59cf0d0c
Jhonathan Geovanny Llanos Gonzalez <jhonathan.llanos@udla.edu.ec>
2020-12-04 22:26:28 -0500

    Agregar archivos de proyecto.

---
+++ XamGMaps/Constants/AppConstants.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace XamGMaps.Constants
+{
+    public class AppConstants
+    {
+        public const string GoogleMapsApiKey = "AIzaSyCx29E70-ZbulVqhjSq4JAn0N2AxzVowik";
+    }
+}
---
+++ XamGMaps/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps;
+
+namespace XamGMaps
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Pin pinTokyo = new Pin()
+            {
+                Type = PinType.Place,
+                Label = "Udla",
+                Address = "Udla, AV. granados, Av. de los colimes",
+                Position = new Position(-0.16720865592050496, -78.47262167400834),
+                Rotation = 33.3f,
+                Tag = "id_udla",
+
+            };
+            map.Pins.Add(pinTokyo);
+            map.MoveToRegion(MapSpan.FromCenterAndRadius(pinTokyo.Position, Distance.FromMeters(5000)));
+
+        }
+    }
+}
