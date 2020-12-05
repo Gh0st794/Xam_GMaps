8267cba242e28b31581291e52d19be1f7603fcf4
Jhonathan Geovanny Llanos Gonzalez <jhonathan.llanos@udla.edu.ec>
2020-12-04 22:52:34 -0500

    Cambiar ApiKey

--- XamGMaps/Constants/AppConstants.cs
+++ XamGMaps/Constants/AppConstants.cs
@@ -3,6 +3,6 @@ namespace XamGMaps.Constants
 {
     public class AppConstants
     {
-        public const string GoogleMapsApiKey = "AIzaSyCx29E70-ZbulVqhjSq4JAn0N2AxzVowik";
+        public const string GoogleMapsApiKey = "Api_Key";
     }
 }
